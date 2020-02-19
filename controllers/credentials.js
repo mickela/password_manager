ac932adc5d28a1f663c14c7d27aca619d1419f7e
Michael <osoba.michael@outlook.com>
2020-02-19 10:07:56 +0100

    Fixed bug that allowed user create credential with empty form values

--- controllers/credentials.js
+++ controllers/credentials.js
@@ -6,24 +6,31 @@ const User = require('../models/user');
 exports.newCred = function(req, res, next){
     const { app, login, alt_login, key, user_id } = req.body;
 
-    User.findAll({
-        where: { id: user_id }
-    }).then(user =>{
-        if(user.length > 0){
-            Credential.create({
-                user_id: user_id,
-                app: app,
-                login: login,
-                key: key,
-                altLogin: alt_login
-            }).then(cred =>{
-                console.log(cred)
-                res.json({ status: true, msg: 'created new credential' });
-            })
-        }else{
-            res.json({ status: false, msg: 'Found no user with that id' });
-        }
-    })
+    if( app.length > 1 && login.length > 1 && alt_login.length > 1 && key.length > 1 && user_id.length !== 0 ){
+        
+        User.findAll({
+            where: { id: user_id }
+        }).then(user =>{
+            if(user.length > 0){
+                Credential.create({
+                    user_id: user_id,
+                    app: app,
+                    login: login,
+                    key: key,
+                    altLogin: alt_login
+                }).then(cred =>{
+                    console.log(cred)
+                    res.json({ status: true, msg: 'created new credential' });
+                })
+            }else{
+                res.json({ status: false, msg: 'Found no user with that id' });
+            }
+        })
+        
+    }else{
+        res.json({ status: false, msg: 'Form can not be empty' });
+    }
+
 }
 
 exports.fetchCreds = function(req, res,next){
